1f1d12247211274e389e276af07556f350ae7e63
Santiago Peña Nieto <santip2357@gmail.com>
2024-05-04 10:08:05 -0500

    Initial commit

---
+++ generador.py
@@ -0,0 +1,33 @@
+from sys import stdin
+import random as r
+
+def main():
+    l = []
+    m = []
+    o = []
+    for i in range(10):
+        l.append(1 if r.random() >= 0.5 else -1)
+    for i in range(10):
+        m.append({})
+        y = 0
+        while len(m[i]) != 8:
+            x = r.randint(0,9)
+            if x != i:
+                if not x in m[i]:
+                    m[i][x] = l[x]
+                    if l[x] > 0:
+                        y += 1
+        o.append((2 * (y / 8)) - 1)
+    for i in range(10):
+        print("< %d : [\"o\", %1.1f] [\"g\", %1.1f]" % (i, o[i], l[i]), end="")
+        for j in m[i]:
+            print(" [\"%d\", %1.1f]" % (j, m[i][j]), end="")
+        print(" > ,")
+
+def mai():
+    for i in range(10):
+        for j in range(10):
+            if j != i:
+                print("< (%d , %d) : 1.0 > ," % (i, j))
+
+mai()
