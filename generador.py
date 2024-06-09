bae0d13956b94a0557b44f8bcc141d5ba375a88d
Santiago Peña Nieto <santip2357@gmail.com>
2024-06-09 13:45:38 -0500

    Se hicieron más pruebas con grafos especiales

--- generador.py
+++ generador.py
@@ -39,11 +39,91 @@ def edges2(n):
     for i in range(n):
         for j in range(n):
             if j != i:
-                print("< (%d , %d) : %1.2f > ," % (i, j, r.random()), end="")
+                print("< (%d , %d) : %1.2f >, " % (i, j, r.random()), end="")
         print()
     print()
 
-nodes2(10)
-edges2(10)
+#nodes1()
+#edges1()
+#nodes2(10)
+#edges2(5)
+
+def verificar1():
+    o = {0 : 0.38, 1 : -0.71, 2 : 0.55, 3 : 0.92, 4 : -0.86}
+    lst = [((0 , 2), 0.60), ((0 , 4) , 0.23),
+           ((1 , 2), 0.69), ((1 , 3) , 0.45),
+           ((2 , 3), 0.60),
+           ((3 , 0), 0.79), ((3 , 1) , 0.68), ((3 , 4), 0.37),
+           ((4 , 0), 0.74), ((4 , 1) , 0.19)]
+    subsets = [[]]
+    for element in lst:
+        new_subsets = []
+        for subset in subsets:
+            new_subset = subset + [element]
+            new_subsets.append(new_subset)
+        subsets.extend(new_subsets)
+    for se in subsets:
+        o1 = o.copy()
+        for n in o1:
+            le = []
+            w = 0
+            for e in se:
+                if n == e[0][1]:
+                    le.append(e)
+                    w += e[1]
+            for e in le:
+                o1[n] += ((o[e[0][0]] - o[e[0][1]]) * (e[1] / w))
+        c = 0
+        for n in o1:
+            for n1 in o1:
+                if abs(o1[n] - o1[n1]) > 0.01:
+                    c = 1
+                    break
+            if c:
+                break
+        if not c:
+            print("si")
+
+def verificar2():
+    o = {0 : 0.279, 1 : 0.3, 2 : 0.1, 3 : 0.279, 4 : 0.5, 5 : -0.4, 6 : 0.8}
+    lst = [((0 , 1), 0.9478), ((0 , 5), 0.9322),
+           ((1 , 0), 0.6500), ((1 , 4), 0.8154), ((1 , 5), 0.4587),
+           ((2 , 3), 0.0705),
+           ((3 , 2), 0.3792), ((3 , 4), 0.3792), ((3 , 6), 0.9274),
+           ((4 , 0), 0.1261), ((4 , 6), 0.3672),
+           ((5 , 0), 0.0200), ((5 , 1), 0.3429), ((5 , 2), 0.6859),
+           ((6 , 3), 0.0242)]
+    subsets = [[]]
+    for element in lst:
+        new_subsets = []
+        for subset in subsets:
+            new_subset = subset + [element]
+            new_subsets.append(new_subset)
+        subsets.extend(new_subsets)
+    for se in subsets:
+        o1 = o.copy()
+        for n in o1:
+            le = []
+            w = 0
+            for e in se:
+                if n == e[0][1]:
+                    le.append(e)
+                    w += e[1]
+            for e in le:
+                o1[n] += ((o[e[0][0]] - o[e[0][1]]) * (e[1] / w))
+        c = 0
+        for n in o1:
+            for n1 in o1:
+                if abs(o1[n] - o1[n1]) > 0.01:
+                    c = 1
+                    break
+            if c:
+                break
+        if not c:
+            print("si")
+
+verificar1()
+#verificar2()
+
 
 #search [3] init =>* STATE such that consensus(STATE) .
